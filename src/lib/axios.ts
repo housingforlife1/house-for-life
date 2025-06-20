cc1ac83ff747f4083daf0d4b8416819a1d19cfc5
patson120 <patrickkennenl@gmail.com>
2025-06-20 03:35:13 +0100

    INitialisation du projet

---
+++ src/lib/axios.ts
@@ -0,0 +1,60 @@
+// import { User } from '@/app/types';
+import axios from 'axios';
+// import Cookies from 'js-cookie';
+
+
+// Create axios instance with custom config
+let api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL || '',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+    // withCredentials: true, // Important for cookies
+});
+
+// Request interceptor
+api.interceptors.request.use(async (config: any) => {
+    // const currentUser = Cookies.get('user');
+    // const userJson: User | null = currentUser ? JSON.parse(currentUser!) : null
+    
+    try {
+        // // Add token to all request except login route
+        // if (!config.url?.includes("auth") && !config.url?.endsWith("/api/administrateurs") ) {
+        //    config.headers['Authorization'] = `Bearer ${userJson?.token}`;
+        // }
+        return config;
+    } catch (error) {
+        console.error('Error fetching CSRF token:', error);
+        return Promise.reject(error);
+    }
+});
+
+// Response interceptor
+api.interceptors.response.use(
+    (response: any) => response,
+    async (error: any) => {
+        // Handle specific error cases
+        if (error.response?.status === 403) {
+            console.error('CSRF token validation failed');
+        }
+        return Promise.reject(error);
+    }
+);
+
+// API methods
+export const apiClient = {
+    get: <T>(url: string, headers?: Record<string, string>) =>
+        api.get<T>(url, { headers }).then((res: any) => res.data),
+
+    post: <T>(url: string, data: any, headers?: Record<string, string>) =>
+        api.post<T>(url, data, { headers }).then((res: any) => res.data),
+
+    put: <T>(url: string, data: any, headers?: Record<string, string>) =>
+        api.put<T>(url, data, { headers }).then((res: any) => res.data),
+
+    delete: <T>(url: string, headers?: Record<string, string>) =>
+        api.delete<T>(url, { headers }).then((res: any) => res.data),
+};
+
+export default api;
