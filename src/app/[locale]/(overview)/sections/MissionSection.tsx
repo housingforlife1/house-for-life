e2f5ea07d6986e629b0d6a0b1e6f3c2f3f8e794d
patson120 <patrickkennenl@gmail.com>
2025-06-24 17:57:57 +0100

    Mise à jour du responsive de la page d'accueil

--- src/app/[locale]/(overview)/sections/MissionSection.tsx
+++ src/app/[locale]/(overview)/sections/MissionSection.tsx
@@ -8,73 +8,74 @@ export default function MissionSection() {
     // Data for support options
     const supportOptionsData = [
         {
-        icon: "/assets/icons/favourite.png",
-        title: "Faire un don",
-        description:
-            "Votre contribution financière nous aide à construire et entretenir des logements abordables. Chaque dollar compte.",
-        action: "Donner maintenant",
+            icon: "/assets/icons/favourite.png",
+            title: "Faire un don",
+            description:
+                "Votre contribution financière nous aide à construire et entretenir des logements abordables. Chaque dollar compte.",
+            action: "Donner maintenant",
         },
         {
-        icon: "/assets/icons/agreement-01.png",
-        title: "Partenariats",
-        description:
-            "Les entreprises et organisations peuvent s'associer à notre mission par des dons, du mécénat de compétences ou des partenariats stratégiques.",
-        action: "Devenir partenaire",
+            icon: "/assets/icons/agreement-01.png",
+            title: "Partenariats",
+            description:
+                "Les entreprises et organisations peuvent s'associer à notre mission par des dons, du mécénat de compétences ou des partenariats stratégiques.",
+            action: "Devenir partenaire",
         },
         {
-        icon: "/assets/icons/star.png",
-        title: "Devenir bénévole",
-        description:
-            "Mettez vos compétences et votre temps au service de notre mission. Rejoignez notre équipe de bénévoles dévoués.",
-        action: "Rejoindre l'équipe",
+            icon: "/assets/icons/star.png",
+            title: "Devenir bénévole",
+            description:
+                "Mettez vos compétences et votre temps au service de notre mission. Rejoignez notre équipe de bénévoles dévoués.",
+            action: "Rejoindre l'équipe",
         },
     ];
 
     // Data for testimonials
     const testimonialsData = [
         {
-        quote:
-            "Avoir un logement stable et abordable m'a permis de reprendre mes études tout en élevant mes enfants. C'est un nouveau départ pour toute notre famille.",
-        author: "Marie, mère de deux enfants",
+            quote:
+                "Avoir un logement stable et abordable m'a permis de reprendre mes études tout en élevant mes enfants. C'est un nouveau départ pour toute notre famille.",
+            author: "Marie, mère de deux enfants",
         },
         {
-        quote:
-            "Soutenir Housing for Life en tant que bénévole m'a permis de contribuer concrètement à améliorer la vie de femmes et d'enfants de ma communauté. C'est une expérience enrichissante.",
-        author: "Jeanne, bénévole",
+            quote:
+                "Soutenir Housing for Life en tant que bénévole m'a permis de contribuer concrètement à améliorer la vie de femmes et d'enfants de ma communauté. C'est une expérience enrichissante.",
+            author: "Jeanne, bénévole",
         },
         {
-        quote:
-            "Notre entreprise est fière de s'associer à Housing for Life. Leur mission s'aligne parfaitement avec nos valeurs d'inclusion et de responsabilité sociale.",
-        author: "Robert, directeur d'une entreprise partenaire",
+            quote:
+                "Notre entreprise est fière de s'associer à Housing for Life. Leur mission s'aligne parfaitement avec nos valeurs d'inclusion et de responsabilité sociale.",
+            author: "Robert, directeur d'une entreprise partenaire",
         },
     ];
 
     return (
-        <div className="py-[140px] bg-[#f8f8e9] w-full my-32">
+        <div className="py-24 md:py-32 bg-[#f8f8e9] w-full my-32">
             <div className='flex flex-col items-start gap-[120px] max-width'>
+
                 {/* Mission Statement */}
-                <div className="grid grid-cols-12 w-full gap-8">
+                <div className="grid grid-cols-12 w-full gap-8 overflow-y-hidden">
                     <div className="flex flex-col col-span-4 items-start gap-11">
                         <div className="flex flex-col items-start gap-5 w-full">
-                        <div className="flex flex-col items-start gap-2 w-full">
-                            <span className="font-paragraph-2 text-black text-[length:var(--paragraph-2-font-size)] tracking-[var(--paragraph-2-letter-spacing)] leading-[var(--paragraph-2-line-height)] [font-style:var(--paragraph-2-font-style)]">
-                            Notre mission
-                            </span>
-                            <h2 className="font-extrabold text-black text-[length:var(--heading-2-font-size)] tracking-[var(--heading-2-letter-spacing)] leading-[var(--heading-2-line-height)] [font-style:var(--heading-2-font-style)]">
-                            La solidarité commence par un foyer.
-                            </h2>
-                        </div>
-                        <p className="font-paragraph-1 text-gray text-[length:var(--paragraph-1-font-size)] tracking-[var(--paragraph-1-letter-spacing)] leading-[var(--paragraph-1-line-height)] [font-style:var(--paragraph-1-font-style)]">
-                            Habitation pour la vie s&apos;engage à offrir des logements
-                            abordables et un soutien communautaire aux femmes et mères
-                            monoparentales en situation de précarité.
-                        </p>
-                        <p className="font-paragraph-1 text-gray text-[length:var(--paragraph-1-font-size)] tracking-[var(--paragraph-1-letter-spacing)] leading-[var(--paragraph-1-line-height)] [font-style:var(--paragraph-1-font-style)]">
-                            Nous croyons que chaque femme et chaque enfant mérite un
-                            chez-soi sûr, stable et abordable. Notre approche repose sur les
-                            valeurs de solidarité, respect, dignité, inclusion et
-                            transparence.
-                        </p>
+                            <div className="flex flex-col items-start gap-2 w-full">
+                                <span className="font-paragraph-2 text-black text-[length:var(--paragraph-2-font-size)] tracking-[var(--paragraph-2-letter-spacing)] leading-[var(--paragraph-2-line-height)] [font-style:var(--paragraph-2-font-style)]">
+                                Notre mission
+                                </span>
+                                <h2 className="font-extrabold text-black text-[length:var(--heading-2-font-size)] tracking-[var(--heading-2-letter-spacing)] leading-[var(--heading-2-line-height)] [font-style:var(--heading-2-font-style)]">
+                                La solidarité commence par un foyer.
+                                </h2>
+                            </div>
+                            <p className="font-paragraph-1 text-gray text-[length:var(--paragraph-1-font-size)] tracking-[var(--paragraph-1-letter-spacing)] leading-[var(--paragraph-1-line-height)] [font-style:var(--paragraph-1-font-style)]">
+                                Habitation pour la vie s&apos;engage à offrir des logements
+                                abordables et un soutien communautaire aux femmes et mères
+                                monoparentales en situation de précarité.
+                            </p>
+                            <p className="font-paragraph-1 text-gray text-[length:var(--paragraph-1-font-size)] tracking-[var(--paragraph-1-letter-spacing)] leading-[var(--paragraph-1-line-height)] [font-style:var(--paragraph-1-font-style)]">
+                                Nous croyons que chaque femme et chaque enfant mérite un
+                                chez-soi sûr, stable et abordable. Notre approche repose sur les
+                                valeurs de solidarité, respect, dignité, inclusion et
+                                transparence.
+                            </p>
                         </div>
                         <Button className="h-[50px] px-5 py-3.5 bg-green rounded-[54px] text-white font-paragraph-bold text-[length:var(--paragraph-bold-font-size)] tracking-[var(--paragraph-bold-letter-spacing)] leading-[var(--paragraph-bold-line-height)] [font-style:var(--paragraph-bold-font-style)]">
                             Soutenir la&nbsp;&nbsp;mission
@@ -99,12 +100,12 @@ export default function MissionSection() {
                 </div>
 
                 {/* Support Section */}
-                <div className="flex flex-col w-full items-start gap-[34px]">
+                <div className="flex flex-col w-full items-start gap-8">
                     <div className="flex flex-col items-start gap-4 w-full">
-                        <h2 className="font-extrabold text-black text-[length:var(--heading-2-font-size)] tracking-[var(--heading-2-letter-spacing)] leading-[var(--heading-2-line-height)] whitespace-nowrap [font-style:var(--heading-2-font-style)]">
+                        <h2 className="font-extrabold text-black text-[length:var(--heading-2-font-size)] tracking-[var(--heading-2-letter-spacing)] leading-[var(--heading-2-line-height)] [font-style:var(--heading-2-font-style)]">
                             Nous avons besoin de vous.
                         </h2>
-                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full">
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-8 w-full">
                             <div className="flex flex-col items-start gap-4">
                                 <p className="font-paragraph-1 text-gray text-base leading-[25.6px]">
                                 Notre organisme compte sur les subventions de l&apos;État, celles
@@ -127,68 +128,72 @@ export default function MissionSection() {
                         </div>
                     </div>
 
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 h-[340px] w-full">
-                        {supportOptionsData.map((option, index) => (
-                        <Card
-                            key={index}
-                            className="flex flex-col items-start justify-between shadow-none p-5 bg-white rounded-3xl h-full border-none">
-                            <CardContent className="flex flex-col items-start gap-4 p-0 w-full">
-                                <div className="w-[60px] h-[60px] bg-white rounded-[40px] flex items-center justify-center">
-                                    <div className='relative w-[34px] h-[34px]'>
-                                        <Image
-                                            className="object-contain"
-                                            alt={option.title}
-                                            src={option.icon}
-                                            fill
-                                        />
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8 w-full">
+                        {
+                            supportOptionsData.map((option, index) => (
+                                <Card
+                                    key={index}
+                                    className="flex flex-col items-start justify-between shadow-none p-5 bg-white rounded-3xl h-full border-none">
+                                    <CardContent className="flex flex-col items-start gap-4 p-0 w-full">
+                                        <div className="w-[60px] h-[60px] bg-white rounded-[40px] flex items-center justify-center">
+                                            <div className='relative w-[34px] h-[34px]'>
+                                                <Image
+                                                    className="object-contain"
+                                                    alt={option.title}
+                                                    src={option.icon}
+                                                    fill
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="flex flex-col items-start gap-2 w-full">
+                                            <h4 className="font-extrabold text-black text-[length:var(--heading-4-font-size)] tracking-[var(--heading-4-letter-spacing)] leading-[var(--heading-4-line-height)] [font-style:var(--heading-4-font-style)]">
+                                                {option.title}
+                                            </h4>
+                                            <p className="font-paragraph-1 text-gray text-[length:var(--paragraph-1-font-size)] tracking-[var(--paragraph-1-letter-spacing)] leading-[var(--paragraph-1-line-height)] [font-style:var(--paragraph-1-font-style)]">
+                                            {option.description}
+                                            </p>
+                                        </div>
+                                    </CardContent>
+                                    <div className="w-full h-[37px]">
+                                        <div className="w-full h-9 bg-[#fcfcf8] border-t border-neutral-200">
+                                            <div className="flex items-center justify-between w-full mt-[17px]">
+                                            <span className="font-paragraph-bold text-black text-[length:var(--paragraph-bold-font-size)] tracking-[var(--paragraph-bold-letter-spacing)] leading-[var(--paragraph-bold-line-height)] [font-style:var(--paragraph-bold-font-style)] cursor-pointer">
+                                                {option.action}
+                                            </span>
+                                            <ChevronRight className="w-[18px] h-[18px]" />
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="flex flex-col items-start gap-2 w-full">
-                                    <h4 className="font-extrabold text-black text-[length:var(--heading-4-font-size)] tracking-[var(--heading-4-letter-spacing)] leading-[var(--heading-4-line-height)] [font-style:var(--heading-4-font-style)]">
-                                        {option.title}
-                                    </h4>
-                                    <p className="font-paragraph-1 text-gray text-[length:var(--paragraph-1-font-size)] tracking-[var(--paragraph-1-letter-spacing)] leading-[var(--paragraph-1-line-height)] [font-style:var(--paragraph-1-font-style)]">
-                                    {option.description}
-                                    </p>
-                                </div>
-                            </CardContent>
-                            <div className="w-full h-[37px]">
-                                <div className="w-full h-9 bg-[#fcfcf8] border-t border-neutral-200">
-                                    <div className="flex items-center justify-between w-full mt-[17px]">
-                                    <span className="font-paragraph-bold text-black text-[length:var(--paragraph-bold-font-size)] tracking-[var(--paragraph-bold-letter-spacing)] leading-[var(--paragraph-bold-line-height)] [font-style:var(--paragraph-bold-font-style)] cursor-pointer">
-                                        {option.action}
-                                    </span>
-                                    <ChevronRight className="w-[18px] h-[18px]" />
-                                    </div>
-                                </div>
-                            </div>
-                        </Card>
-                        ))}
+                                </Card>
+                            ))
+                        }
                     </div>
                 </div>
 
                 {/* Testimonials Section */}
                 <div className="flex flex-col items-start gap-4 w-full">
-                <h2 className="font-extrabold text-black text-[length:var(--heading-2-font-size)] tracking-[var(--heading-2-letter-spacing)] leading-[var(--heading-2-line-height)] whitespace-nowrap [font-style:var(--heading-2-font-style)]">
+                <h2 className="font-extrabold text-black text-[length:var(--heading-2-font-size)] tracking-[var(--heading-2-letter-spacing)] leading-[var(--heading-2-line-height)] [font-style:var(--heading-2-font-style)]">
                     Ils parlent de nous
                 </h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full">
-                    {testimonialsData.map((testimonial, index) => (
-                    <div
-                        key={index}
-                        className="flex flex-col items-start gap-4">
-                        <p className="font-paragraph-1 text-gray text-base leading-4">
-                        <span className="leading-[var(--paragraph-1-line-height)] font-paragraph-1 [font-style:var(--paragraph-1-font-style)] font-[number:var(--paragraph-1-font-weight)] tracking-[var(--paragraph-1-letter-spacing)] text-[length:var(--paragraph-1-font-size)]">
-                            {testimonial.quote}
-                            <br />
-                        </span>
-                        <span className="font-bold [font-style:var(--paragraph-bold-font-style)] tracking-[var(--paragraph-bold-letter-spacing)] leading-[var(--paragraph-bold-line-height)] text-[length:var(--paragraph-bold-font-size)]">
-                            <br />
-                            {testimonial.author}
-                        </span>
-                        </p>
-                    </div>
-                    ))}
+                <div className="flex flex-row overflow-x-scroll lg:overflow-x-hidden xl:grid-cols-3 gap-4 md:gap-8 w-full h-scroll [&::-webkit-scrollbar]:h-0 pb-6">
+                    {
+                        testimonialsData.map((testimonial, index) => (
+                            <div
+                                key={index}
+                                className="min-w-[250px] md:min-w-[320px] xl:w-full flex flex-col items-start gap-4">
+                                <p className="font-paragraph-1 text-gray text-base leading-4">
+                                    <span className="leading-[var(--paragraph-1-line-height)] font-paragraph-1 [font-style:var(--paragraph-1-font-style)] font-[number:var(--paragraph-1-font-weight)] tracking-[var(--paragraph-1-letter-spacing)] text-[length:var(--paragraph-1-font-size)]">
+                                        {testimonial.quote}
+                                        <br />
+                                    </span>
+                                    <span className="font-bold [font-style:var(--paragraph-bold-font-style)] tracking-[var(--paragraph-bold-letter-spacing)] leading-[var(--paragraph-bold-line-height)] text-[length:var(--paragraph-bold-font-size)]">
+                                        <br />
+                                        {testimonial.author}
+                                    </span>
+                                </p>
+                            </div>
+                        ))
+                    }
                 </div>
                 </div>
             </div>
--- src/app/[locale]/(overview)/sections/NewsSection.tsx
+++ src/app/[locale]/(overview)/sections/NewsSection.tsx
@@ -28,59 +28,60 @@ export default function NewsSection() {
     ]
 
     return (
-        <div className="flex flex-col w-full max-width items-start gap-6 mb-32">
+        <div className="flex flex-col w-full max-width items-start gap-6 my-14 md:mb-32">
             <div className="flex items-center justify-between w-full">
-            <div className="flex flex-col items-start gap-2 w-[390px]">
-                <h2 className="font-extrabold text-black text-[length:var(--heading-2-font-size)] tracking-[var(--heading-2-letter-spacing)] leading-[var(--heading-2-line-height)] [font-style:var(--heading-2-font-style)]">
-                    Dernières actualités
-                </h2>
-            </div>
-            <span className="font-bold text-black text-[length:var(--paragraph-bold-font-size)] tracking-[var(--paragraph-bold-letter-spacing)] leading-[var(--paragraph-bold-line-height)] [font-style:var(--paragraph-bold-font-style)] cursor-pointer">
-                Voir toute l&apos;actualité
-            </span>
-            </div>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full">
-            {newsArticlesData.map((article, index) => (
-                <div
-                    key={index}
-                    className="flex flex-col items-start gap-6">
-                        <div className='relative h-80 w-full overflow-hidden rounded-2xl'>
-                            <Image
-                                className="object-cover"
-                                alt={article.title}
-                                src={article.image}
-                                fill
-                                priority
-                            />
-                        </div>
-                    <div className="flex flex-col items-start gap-4 w-full">
-                        <p className="font-paragraph-1 text-gray text-[length:var(--paragraph-1-font-size)] tracking-[var(--paragraph-1-letter-spacing)] leading-[var(--paragraph-1-line-height)] [font-style:var(--paragraph-1-font-style)]">
-                            {article.title}
-                        </p>
-                        <div className="flex items-center justify-between w-full">
-                            <div className="flex-1 font-normal text-sm leading-[14px]">
-                                <span className="text-[#282724] leading-[22.4px]">
-                                    Par{" "}
-                                </span>
-                                <span className="font-[number:var(--paragraph-2-font-weight)] text-[#282724] leading-[var(--paragraph-2-line-height)] underline font-paragraph-2 [font-style:var(--paragraph-2-font-style)] tracking-[var(--paragraph-2-letter-spacing)] text-[length:var(--paragraph-2-font-size)]">
-                                    {article.source}
-                                </span>
-                            </div>
-                            <div className="flex-1 font-normal text-sm text-right leading-[14px]">
-                                <a
-                                    href={article.link}
-                                    rel="noopener noreferrer"
-                                    target="_blank">
-                                    <span className="font-bold leading-[var(--paragraph-2-line-height)] underline font-paragraph-2 [font-style:var(--paragraph-2-font-style)] tracking-[var(--paragraph-2-letter-spacing)] text-[length:var(--paragraph-2-font-size)]">
-                                        Consulter
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                <div className="flex flex-col items-start gap-2 md:w-[390px]">
+                    <h2 className="font-extrabold text-black text-[length:var(--heading-2-font-size)] tracking-[var(--heading-2-letter-spacing)] leading-[var(--heading-2-line-height)] [font-style:var(--heading-2-font-style)]">
+                        Dernières actualités
+                    </h2>
                 </div>
-            ))}
+                <span className="hidden md:block font-bold text-black text-[length:var(--paragraph-bold-font-size)] tracking-[var(--paragraph-bold-letter-spacing)] leading-[var(--paragraph-bold-line-height)] [font-style:var(--paragraph-bold-font-style)] cursor-pointer">
+                    Voir toute l&apos;actualité
+                </span>
+            </div>
+            <div className="flex flex-row overflow-x-scroll lg:overflow-x-hidden xl:grid-cols-3 gap-4 md:gap-8 w-full h-scroll [&::-webkit-scrollbar]:h-0 pb-6">
+                {
+                    newsArticlesData.map((article, index) => (
+                        <div
+                            key={index}
+                            className="min-w-[250px] md:min-w-[320px] xl:w-full flex flex-col items-start gap-6">
+                            <div className='relative h-80 w-full overflow-hidden rounded-2xl'>
+                                <Image
+                                    className="object-cover"
+                                    alt={article.title}
+                                    src={article.image}
+                                    fill
+                                    priority
+                                />
+                            </div>
+                            <div className="flex flex-col items-start gap-4 w-full">
+                                <p className="font-paragraph-1 text-gray text-[length:var(--paragraph-1-font-size)] tracking-[var(--paragraph-1-letter-spacing)] leading-[var(--paragraph-1-line-height)] [font-style:var(--paragraph-1-font-style)]">
+                                    {article.title}
+                                </p>
+                                <div className="flex items-center justify-between w-full">
+                                    <div className="flex-1 font-normal text-sm leading-[14px]">
+                                        <span className="text-[#282724] leading-[22.4px]">
+                                            Par{" "}
+                                        </span>
+                                        <span className="font-[number:var(--paragraph-2-font-weight)] text-[#282724] leading-[var(--paragraph-2-line-height)] underline font-paragraph-2 [font-style:var(--paragraph-2-font-style)] tracking-[var(--paragraph-2-letter-spacing)] text-[length:var(--paragraph-2-font-size)]">
+                                            {article.source}
+                                        </span>
+                                    </div>
+                                    <div className="flex-1 font-normal text-sm text-right leading-[14px]">
+                                        <a
+                                            href={article.link}
+                                            rel="noopener noreferrer"
+                                            target="_blank">
+                                            <span className="font-bold leading-[var(--paragraph-2-line-height)] underline font-paragraph-2 [font-style:var(--paragraph-2-font-style)] tracking-[var(--paragraph-2-letter-spacing)] text-[length:var(--paragraph-2-font-size)]">
+                                                Consulter
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
