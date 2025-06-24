d4498b7fef75a2bd7980f4f5c5b5d5c15bc3a52b
patson120 <patrickkennenl@gmail.com>
2025-06-24 15:25:42 +0100

    Mise à jour du responsive de la page d'accueil

--- src/app/[locale]/(overview)/sections/OurImpactSection.tsx
+++ src/app/[locale]/(overview)/sections/OurImpactSection.tsx
@@ -30,8 +30,8 @@ export default function OurImpactSection() {
     ];
 
     return (
-        <div className="grid grid-cols-8 max-width gap-10 my-32">
-            <div className='col-span-3 relative h-full rounded-3xl overflow-hidden'>
+        <div className="grid grid-cols-1 md:grid-cols-8 max-width gap-10 my-16 md:my-32">
+            <div className='col-span-full lg:col-span-3 relative h-[380px] lg:h-full rounded-3xl overflow-hidden'>
                 <Image
                     className="object-cover"
                     alt="Image"
@@ -40,50 +40,52 @@ export default function OurImpactSection() {
                 />
             </div>
 
-            <div className="col-span-5 grid grid-cols-2 gap-8">
-                <div className="flex flex-col  items-start gap-[34px]">
+            <div className="col-span-full lg:col-span-5 grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8">
+                <div className="flex flex-col items-start gap-6 lg:gap-8">
                     <div className="flex flex-col items-start gap-2 w-full">
                         <h2 className="font-heading-2 font-extrabold text-black text-[length:var(--heading-2-font-size)] tracking-[var(--heading-2-letter-spacing)] leading-[var(--heading-2-line-height)] [font-style:var(--heading-2-font-style)]">
-                        Notre impact
+                            Notre impact
                         </h2>
                         <p className="font-paragraph-1 text-gray text-[length:var(--paragraph-1-font-size)] tracking-[var(--paragraph-1-letter-spacing)] leading-[var(--paragraph-1-line-height)] [font-style:var(--paragraph-1-font-style)]">
-                        Depuis notre création en 2023, nous travaillons sans relâche
-                        pour offrir des solutions de logement durables et adaptées aux
-                        besoins des femmes et familles monoparentales du
-                        Nouveau-Brunswick.
+                            Depuis notre création en 2023, nous travaillons sans relâche
+                            pour offrir des solutions de logement durables et adaptées aux
+                            besoins des femmes et familles monoparentales du
+                            Nouveau-Brunswick.
                         </p>
                     </div>
 
-                    <Button className="h-[50px] px-5 py-3.5 bg-green rounded-[54px] text-white font-paragraph-bold text-[length:var(--paragraph-bold-font-size)] tracking-[var(--paragraph-bold-letter-spacing)] leading-[var(--paragraph-bold-line-height)] [font-style:var(--paragraph-bold-font-style)]">
+                    <Button className="h-[50px] w-full md:w-min mb-4 md:mb-0 px-5 py-3.5 bg-green rounded-[54px] text-white font-paragraph-bold text-[length:var(--paragraph-bold-font-size)] tracking-[var(--paragraph-bold-letter-spacing)] leading-[var(--paragraph-bold-line-height)] [font-style:var(--paragraph-bold-font-style)]">
                         Voir nos programmes
                     </Button>
                 </div>
 
-                {impactCardsData.map((card, index) => (
-                <Card
-                    key={index}
-                    className={`${card.bgColor} rounded-3xl border-none shadow-none ${index === 0 ? "h-[224px]" : "h-[240px]"}`}>
-                    <CardContent className="flex flex-col gap-6 p-5">
-                        <div className='relative w-[34px] h-[34px]'>
-                            <Image
-                                className="object-contain"
-                                alt={card.title}
-                                src={card.icon}
-                                fill
-                                priority
-                            />
-                        </div>
-                        <div className="flex flex-col items-start gap-2 w-full">
-                            <h4 className="font-heading-4 text-black text-[length:var(--heading-4-font-size)] tracking-[var(--heading-4-letter-spacing)] leading-[var(--heading-4-line-height)] [font-style:var(--heading-4-font-style)]">
-                                {card.title}
-                            </h4>
-                            <p className="font-paragraph-1 text-black text-[length:var(--paragraph-1-font-size)] tracking-[var(--paragraph-1-letter-spacing)] leading-[var(--paragraph-1-line-height)] [font-style:var(--paragraph-1-font-style)]">
-                                {card.description}
-                            </p>
-                        </div>
-                    </CardContent>
-                </Card>
-                ))}
+                {
+                    impactCardsData.map((card, index) => (
+                        <Card
+                            key={index}
+                            className={`${card.bgColor} rounded-3xl border-none shadow-none ${index === 0 ? "min-h-[224px]" : "min-h-[240px]"}`}>
+                            <CardContent className="flex flex-col gap-6 p-5">
+                                <div className='relative w-[34px] h-[34px]'>
+                                    <Image
+                                        className="object-contain"
+                                        alt={card.title}
+                                        src={card.icon}
+                                        fill
+                                        priority
+                                    />
+                                </div>
+                                <div className="flex flex-col items-start gap-2 w-full">
+                                    <h4 className="font-heading-4 text-black text-[length:var(--heading-4-font-size)] tracking-[var(--heading-4-letter-spacing)] leading-[var(--heading-4-line-height)] [font-style:var(--heading-4-font-style)]">
+                                        {card.title}
+                                    </h4>
+                                    <p className="font-paragraph-1 text-black text-[length:var(--paragraph-1-font-size)] tracking-[var(--paragraph-1-letter-spacing)] leading-[var(--paragraph-1-line-height)] [font-style:var(--paragraph-1-font-style)]">
+                                        {card.description}
+                                    </p>
+                                </div>
+                            </CardContent>
+                        </Card>
+                    ))
+                }
             </div>
         </div>
     )
