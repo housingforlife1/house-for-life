b9fe36a8c4c6bc896540d909ba99e59ac7f2e783
patson120 <patrickkennenl@gmail.com>
2025-06-25 08:26:31 +0100

    Intégration du responsive de la page des programmes

--- src/app/[locale]/(overview)/about/Sections/ValuesSection.tsx
+++ src/app/[locale]/(overview)/about/Sections/ValuesSection.tsx
@@ -36,26 +36,28 @@ export default function ValuesSection() {
           </h2>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 w-full">
-          {values.map((value, index) => (
-            <div key={index} className="w-full flex flex-col items-start gap-4 border-b border-blue-100 md:border-none pb-8 md:pb-0">
-                <div className='relative w-[34px] h-[34px]'>
-                    <Image
-                      className="object-contain"
-                      alt={value.title}
-                      src={value.icon}
-                      fill
-                    />
+          {
+            values.map((value, index) => (
+              <div key={index} className="w-full flex flex-col items-start gap-4 border-b border-blue-100 md:border-none pb-8 md:pb-0">
+                  <div className='relative w-[34px] h-[34px]'>
+                      <Image
+                        className="object-contain"
+                        alt={value.title}
+                        src={value.icon}
+                        fill
+                      />
+                  </div>
+                <div className="flex flex-col items-start gap-1 w-full">
+                  <h4 className="font-heading-4 font-[number:var(--heading-4-font-weight)] text-black text-[length:var(--heading-4-font-size)] tracking-[var(--heading-4-letter-spacing)] leading-[var(--heading-4-line-height)] [font-style:var(--heading-4-font-style)]">
+                    {value.title}
+                  </h4>
+                  <p className="font-paragraph-1 font-[number:var(--paragraph-1-font-weight)] text-gray text-[length:var(--paragraph-1-font-size)] tracking-[var(--paragraph-1-letter-spacing)] leading-[var(--paragraph-1-line-height)] [font-style:var(--paragraph-1-font-style)]">
+                    {value.description}
+                  </p>
                 </div>
-              <div className="flex flex-col items-start gap-1 w-full">
-                <h4 className="font-heading-4 font-[number:var(--heading-4-font-weight)] text-black text-[length:var(--heading-4-font-size)] tracking-[var(--heading-4-letter-spacing)] leading-[var(--heading-4-line-height)] [font-style:var(--heading-4-font-style)]">
-                  {value.title}
-                </h4>
-                <p className="font-paragraph-1 font-[number:var(--paragraph-1-font-weight)] text-gray text-[length:var(--paragraph-1-font-size)] tracking-[var(--paragraph-1-letter-spacing)] leading-[var(--paragraph-1-line-height)] [font-style:var(--paragraph-1-font-style)]">
-                  {value.description}
-                </p>
               </div>
-            </div>
-          ))}
+            ))
+          }
         </div>
     </div>
     )
