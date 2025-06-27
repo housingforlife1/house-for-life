5bbfef3e0f29c3df0873b24ff48b3e66036044e6
patson120 <patrickkennenl@gmail.com>
2025-06-27 06:11:59 +0100

    Mise à jour

--- src/app/[locale]/(other)/contact/sections/ContactFormSection.tsx
+++ src/app/[locale]/(other)/contact/sections/ContactFormSection.tsx
@@ -1,14 +1,14 @@
 "use client"
 
-import React from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import React from "react";
 import { useForm } from "react-hook-form";
+import * as z from "zod";
 
 
 const formSchema = z.object({
