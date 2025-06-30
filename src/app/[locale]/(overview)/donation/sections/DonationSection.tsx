77b849b1067a14f5376497fb6daa58953a799592
patson120 <patrickkennenl@gmail.com>
2025-06-30 12:12:39 +0100

    Ajout du formulaire zeffy pour les dons

--- src/app/[locale]/(overview)/donation/sections/DonationSection.tsx
+++ src/app/[locale]/(overview)/donation/sections/DonationSection.tsx
@@ -171,13 +171,6 @@ export default function DonationSection() {
                     </div>
                 </div> 
             </div>
-            <div className='relative overflow-x-hidden h-screen w-full'>
-                <iframe 
-                    title='Donation form powered by Zeffy' 
-                    style={{ position: 'absolute', border: 0, top:0,left:0,bottom:0,right:0,width:'100%',height:'100%' }} 
-                    src='https://www.zeffy.com/embed/donation-form/soutenez-notre-mission-57' allowTransparency={true}>
-                </iframe>
-            </div>
         </>
     )
 }
