53cd2616bde5d76a7a4ef5148c1854135d8fd328
patson120 <patrickkennenl@gmail.com>
2025-07-02 18:06:23 +0100

    Mise à jour de la page des dons

--- src/app/[locale]/(overview)/donation/sections/DonationSection.tsx
+++ src/app/[locale]/(overview)/donation/sections/DonationSection.tsx
@@ -117,7 +117,7 @@ export default function DonationSection() {
                     <div className='relative overflow-x-hidden h-full w-full'>
                         <iframe 
                             title='Donation form powered by Zeffy' 
-                            style={{ position: 'absolute', border: 0, top:0,left:0,bottom:0,right:0,width:'100%',height:'100%' }} 
+                            style={{ position: 'absolute', border: 0, top:0,left:0,bottom:0,right:0,width:'100%', height:'100%' }} 
                             src='https://www.zeffy.com/embed/donation-form/soutenez-notre-mission-57' allowTransparency={true}>
                         </iframe>
                     </div>
