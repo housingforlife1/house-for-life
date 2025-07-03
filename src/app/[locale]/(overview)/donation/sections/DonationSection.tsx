a53ccca82719707bffc1c10f560835df7d9f4a7e
patson120 <patrickkennenl@gmail.com>
2025-07-03 08:44:43 +0100

    Mise à jour du formulaire de don

--- src/app/[locale]/(overview)/donation/sections/DonationSection.tsx
+++ src/app/[locale]/(overview)/donation/sections/DonationSection.tsx
@@ -1,7 +1,4 @@
-// import { Button } from '@/components/ui/button'
 import { Card, CardContent } from '@/components/ui/card'
-// import { Checkbox } from '@/components/ui/checkbox'
-// import { Input } from '@/components/ui/input'
 import { Separator } from '@/components/ui/separator'
 import { useLocale, useTranslations } from 'next-intl'
 
