170c1da6a72702b510ba9f5587418bf09bca19b7
patson120 <patrickkennenl@gmail.com>
2025-08-22 19:18:56 +0100

    Mise à jour de l'url du backend pour l'envoi des mails

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
-const nextConfig: NextConfig = {
-    // output: "export",
-    distDir: "build"
-};
+const nextConfig: NextConfig = {};
  
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
